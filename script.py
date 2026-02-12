c78b7ee09196eda692382d573a9ba6824c54fb20
SashaCurry <alekseev.aleksandr01@mail.ru>
2026-02-13 00:57:25 +0400

    Features | Numpy practice is done

--- script.py
+++ script.py
@@ -1,1 +1,14 @@
-print(2**3)
+import numpy as np
+
+array = np.zeros(100, dtype=bool)
+simples = np.empty(0, dtype=int)
+for i in range (2, 101):
+    is_simple = True
+    for simple in simples:
+        if i % simple == 0:
+            is_simple = False
+            break
+    if is_simple:
+        array[i - 1] = True
+        simples = np.append(simples, i)
+print(array)
